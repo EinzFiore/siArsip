81d8371607ecdb7e1ffa63f4807942ba4786d9fe
EinzFiore <ahmadjunaedi1412@gmail.com>
2020-10-18 20:35:19 +0700

    delete routes

--- routes/web.php
+++ routes/web.php
@@ -40,23 +40,3 @@ Route::post('batch.create', 'BatchController@tambahBatch')->name('tambahBatch');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-Route::get('/routes', function () {
-    $routeCollection = Route::getRoutes();
-    echo "<table style='width:100%'>";
-    echo "<tr>";
-    echo "<td width='10%'><h4>HTTP Method</h4></td>";
-    echo "<td width='10%'><h4>Route</h4></td>";
-    echo "<td width='10%'><h4>Name</h4></td>";
-    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
-    echo "</tr>";
-    foreach ($routeCollection as $value) {
-        echo "<tr>";
-        echo "<td>" . $value->methods()[0] . "</td>";
-        echo "<td>" . $value->uri() . "</td>";
-        echo "<td>" . $value->getName() . "</td>";
-        echo "<td>" . $value->getActionName() . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-});
